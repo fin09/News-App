cc846e2856ff49e21a596dd31cc76278dd2c1270
fin09 <fadysameh22m@gmail.com>
2026-02-19 20:44:30 +0200

    add taskbar to app

---
+++ lib/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 24.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(Icons.newspaper),
+          ],
+        ),
+      ),
+      body: Center(child: Text('Home')),
+    );
+  }
+}
